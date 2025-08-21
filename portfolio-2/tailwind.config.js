7050d7a0682ecfedd209a25af59d4266f7bde653
oshanmanidu <oshanmanidu12@gmail.com>
2025-08-21 11:41:03 +0530

    configs

--- portfolio-2/tailwind.config.js
+++ portfolio-2/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
